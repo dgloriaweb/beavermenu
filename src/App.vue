43719aa5feb96784e08ac4046ec7c70557adb228
unknown <dgloria.web@gmail.com>
2021-01-14 16:21:51 +0000

    fixing vue chrome

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-   
+   <div id="nav">
+    <router-link to="/">Meals List</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
   </div>
 </template>
 
